4e9f44c8f45994c5348a7485474e220dc23bd7ca
i7i8i9com <752399672@qq.com>
2022-11-27 14:33:51 +0800

    add interface

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 import './style.css'
 import App from './App.vue'
+const myApp=createApp(App)
+myApp.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
 
-createApp(App).mount('#app')
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app')
